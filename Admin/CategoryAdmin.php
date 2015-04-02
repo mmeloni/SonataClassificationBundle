e7ecd5818f0d0a083e9c0826fa2296498a337bcc
michele meloni <cleaversdev@gmail.com>
2015-04-02 10:52:05 +0200

    Update CategoryAdmin.php

--- Admin/CategoryAdmin.php
+++ Admin/CategoryAdmin.php
@@ -30,6 +30,7 @@ class CategoryAdmin extends Admin
         $formMapper
             ->with('General', array('class' => 'col-md-6'))
                 ->add('name')
+                ->add('enabled')
                 ->add('description', 'textarea', array('required' => false))
             ->end()
             ->with('Options', array('class' => 'col-md-6'))
